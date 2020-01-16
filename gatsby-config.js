c7f08db993446cc0c50473cb01ea77666155880a
Sinan <me@wunnle.com>
2020-01-17 02:03:07 +0300

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Wunnle Blog`,
     description: `A blog about front-end development, design and maybe some short stories.`,
-    author: `@wunle`
+    author: `@wunnle`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,19 +26,19 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Wunnle's blog`,
+        short_name: `wunnle blog`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/logomark.svg`
       }
     },
     {
       resolve: 'gatsby-transformer-remark',
       options: {
-        plugins: [`gatsby-remark-reading-time`] // just in case those previously mentioned remark plugins sound cool :)
+        plugins: [`gatsby-remark-reading-time`]
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
