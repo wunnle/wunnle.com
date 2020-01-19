9ca29edd174414e37cce4d76b83d5e84bf0aa9f8
Sinan <me@wunnle.com>
2020-01-19 04:46:22 +0300

    accesibility changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,13 +4,11 @@ import styles from './Header.module.css'
 import logo from './logo.svg'
 
 const Header = () => {
-  console.log({ styles })
-
   return (
     <header className={styles.header}>
       <div className={styles.headerInner}>
         <a href="/">
-          <img className={styles.logo} src={logo} width="150" alt="" />
+          <img className={styles.logo} src={logo} width="150" alt="wunnle logo" />
         </a>
         <nav className={styles.nav}>
           <a href="https://blog.wunnle.com/">Blog</a>
