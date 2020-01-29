9934f03856cbafe053179ad113fdf7abe8e06ede
wunnle <me@wunnle.com>
2020-01-29 09:50:51 +0300

    remove graphql from layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,5 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import './layout.css'
 
-import { graphql, useStaticQuery } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
@@ -15,19 +7,9 @@ import Footer from '../Footer/index'
 import Header from '../Header/index'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <div className="wrapper">
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header />
       <main>{children}</main>
       <Footer />
     </div>
