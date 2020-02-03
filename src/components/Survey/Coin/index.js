232ed086f1030108b564a012d4e363e7cace3ddb
wunnle <me@wunnle.com>
2020-02-04 00:59:32 +0300

    removed survey

--- src/components/Survey/Coin/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import styles from './Coin.module.css'
-import coin from './coin.png'
-
-const Coin = () => <img src={coin} className={styles.coin} alt="Reach coin" />
-
-export default Coin
--- src/pages/survey.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import Survey from '../components/Survey'
-
-const SurveyPage = () => <Survey />
-
-export default Survey
